7d05a2fb3620243548f3d02acfa2f3e86e7ef3bd
venkatasailakkakula <19kq1a0550@gmail.com>
2021-06-29 15:11:00 +0530

    updating

---
+++ package/logics.py
@@ -0,0 +1,12 @@
+class Calci:
+    def add(n1,n2):
+        return n1+n2
+    
+    def sub(n1,n2):
+        return n1-n2
+    
+    def mul(n1,n2):
+        return n1*n2
+    
+    def div(n1,n2):
+        return n1/n2
